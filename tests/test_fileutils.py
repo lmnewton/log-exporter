43ec66162dd01b7454d7bf2b3c5926f8782af91c
lmnewton <lindsay.marie.newton@gmail.com>
2024-07-04 17:52:37 -0400

    Adding test log files for unit testing.

--- tests/test_fileutils.py
+++ tests/test_fileutils.py
@@ -3,28 +3,28 @@ from app.utils.fileutils import parse_file
 
 def test_readfile():
     lines = []
-    for line in parse_file("tests/resources/test.log", 4):
+    for line in parse_file("tests/resources/test.testlog", 4):
         lines.append(str(line).strip())
     assert lines == ["0", "1 test", "21", "321"]
 
 
 def test_readfile_limited():
     lines = []
-    for line in parse_file("tests/resources/test.log", 3):
+    for line in parse_file("tests/resources/test.testlog", 3):
         lines.append(str(line).strip())
     assert lines == ["0", "1 test", "21"]
 
 
 def test_emptyfile():
     lines = []
-    for line in parse_file("tests/resources/empty.log", 1):
+    for line in parse_file("tests/resources/empty.testlog", 1):
         lines.append(str(line).strip())
     assert len(lines) == 0
 
 
 def test_bufferedread():
     lines = []
-    for line in parse_file("tests/resources/buffered.log", 32):
+    for line in parse_file("tests/resources/buffered.testlog", 32):
         lines.append(str(line))
     assert len(lines) == 32
 
