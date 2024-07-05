53c86cbf96c5c1d22916860394720cc09a3437bd
lmnewton <lindsay.marie.newton@gmail.com>
2024-07-05 12:52:42 -0400

    Fixed a few bugs with how the file pointer was missing data over buffered boundaries. Added additional unit tests to cover these boundary cases.

--- app/utils/fileutils.py
+++ app/utils/fileutils.py
@@ -17,36 +17,44 @@ def parse_file(
         # Keep track of how many lines we've pulled for response
         cumulative_count = 0
 
+        scrollback = 0
+
         while (
             current_position := file_wrapper.tell()
         ) != 0 and cumulative_count < line_count:
-
+            
+            # Ensure we only read in as much as we need when we are on the last buffered read
+            read_size = buffer_size
+            if current_position < buffer_size:
+                read_size = current_position
+            
             # Ensure as we seek that we never attempt to seek to a negative index (top of file 0 is min)
             read_position = max(0, current_position - buffer_size)
             file_wrapper.seek(read_position, os.SEEK_SET)
 
             # Read data into the byte buffer
-            binary_data = file_wrapper.read(buffer_size)
+            binary_data = file_wrapper.read(read_size)
 
             # Split the data at line breaks
             log_chunk = binary_data.splitlines(True)
 
             # We remove the last element in case of the read being caught mid-line.
             # We only need to worry about this in the case of reads across multiple buffers.
-            if read_position != 0:
+            scrollback = 0
+            if read_position > 0:
                 log_chunk.pop(0)
 
-                binary_data = b"".join(log_chunk)
+                ## Track how much we have to scroll back to capture all the data.
+                scrollback = len(binary_data) - len(b"".join(log_chunk))
 
-            # Move the file pointer up from current location to the next read location
-            file_wrapper.seek(
-                max(0, file_wrapper.tell() - len(binary_data)), os.SEEK_SET
-            )
             while len(log_chunk) > 0:
-
                 log_chunk_elem = log_chunk.pop().decode("utf-8")
                 if search_term is None or search_term in log_chunk_elem:
                     yield log_chunk_elem
                     cumulative_count += 1
                 if cumulative_count >= line_count:
                     return
+                
+            # Move the file pointer up from current location to the next read location
+            new_position = max(scrollback, read_position - len(binary_data))
+            file_wrapper.seek(new_position, os.SEEK_SET)
--- tests/test_fileutils.py
+++ tests/test_fileutils.py
@@ -25,8 +25,67 @@ def test_emptyfile():
 def test_bufferedread():
     lines = []
     for line in parse_file("tests/resources/buffered.testlog", 32):
-        lines.append(str(line))
+        lines.append(str(line).strip())
     assert len(lines) == 32
 
+    for line in lines:
+        assert line.startswith("2024-07-04 13:47:44")
+
+def test_bufferedsearch():
+    lines = []
+    for line in parse_file("tests/resources/buffered.testlog", 1000, "QB"):
+        lines.append(str(line).strip())
+    
+    assert len(lines) == 2
+
+def test_limitedbufferedsearch():
+    lines = []
+    for line in parse_file("tests/resources/buffered.testlog", 1, "QB"):
+        lines.append(str(line).strip())
+    
+    assert len(lines) == 1
+
+def test_bufferedsinglehitsearch():
+    lines = []
+    for line in parse_file("tests/resources/buffered.testlog", 1000, "TR"):
+        lines.append(str(line).strip())
+    
+    assert len(lines) == 1
+
+    for line in lines:
+        assert line.startswith("2024-07-04 13:47:44")
+
+def test_bufferednohitsearch():
+    lines = []
+    for line in parse_file("tests/resources/buffered.testlog", 1000, "MKM"):
+        lines.append(str(line))
+    
+    assert len(lines) == 0
+
+    for line in lines:
+        assert line.startswith("2024-07-04 13:47:44")
+
+def test_nobuffersearch():
+    lines = []
+    for line in parse_file("tests/resources/test.testlog", 1000, "1"):
+        lines.append(str(line).strip())
+
+    assert len(lines) == 3
+    assert lines == ["1 test", "21", "321"]
+
+def test_limitednobuffersearch():
+    lines = []
+    for line in parse_file("tests/resources/test.testlog", 2, "1"):
+        lines.append(str(line).strip())
+
+    assert len(lines) == 2
+    assert lines == ["1 test", "21"]
+
+def test_crossbuffersearch():
+    lines = []
+    for line in parse_file("tests/resources/buffered.testlog", 50, "C"):
+        lines.append(str(line).strip())
+
+    assert len(lines) == 23
     for line in lines:
         assert line.startswith("2024-07-04 13:47:44")
